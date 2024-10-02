b4ee23a5bfb592a247888b3b0ca157b108f6613e
kvs8 <kostya.shefer.999@gmail.com>
2024-10-03 02:56:29 +0700

    fix CI: actual python version

--- setup.py
+++ setup.py
@@ -21,8 +21,6 @@ setup(
     install_requires=find_required(),
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
