a614dfea79844709e727fc8c66fd9ed57d648e46
kvs8 <kostya.shefer.999@gmail.com>
2024-11-22 21:04:43 +0700

    Add body response coverage

--- vedro_spec_compare/parser.py
+++ vedro_spec_compare/parser.py
@@ -6,11 +6,18 @@ from schemax_openapi import SchemaData, collect_schema_data
 
 
 class SpecMethod:
-    def __init__(self, method: str, route: str, query_params: List[str], response_codes: List[str]):
+    def __init__(
+            self, method: str,
+            route: str,
+            query_params: List[str],
+            response_codes: List[str],
+            response_schema: Dict[str, Any]
+    ):
         self.method = method
         self.route = route
         self.query_params = query_params
         self.response_codes = response_codes
+        self.response_schema = response_schema
 
     @staticmethod
     def create(data: Any) -> "SpecMethod":
@@ -19,7 +26,8 @@ class SpecMethod:
                 method=data.http_method,
                 route=data.path,
                 query_params=data.queries,
-                response_codes=[str(data.status)]
+                response_codes=[str(data.status)],
+                response_schema=data.response_schema
             )
         else:
             raise ValueError("Unsupported data format")
