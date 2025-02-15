598dc7221f821f28393cf4f299c1e43d96ee17f9
kvs8 <kostya.shefer.999@gmail.com>
2025-02-15 21:25:57 +0700

    Add parse_from_raw

--- vedro_spec_compare/parser.py
+++ vedro_spec_compare/parser.py
@@ -45,30 +45,27 @@ class Parser:
             return cls.parse_from_file(spec_path)
 
     @staticmethod
+    def parse_from_raw(content: str) -> Dict[str, SpecMethod]:
+        data = yaml.load(content, Loader=yaml.CLoader)
+        result = dict()
+        for item in collect_schema_data(data):
+            result[item.interface_method] = SpecMethod.create(item)
+        return result
+
+    @staticmethod
     def parse_from_url(url: str) -> Dict[str, SpecMethod]:
         response = requests.get(url)
 
         if response.status_code != 200:
             raise ValueError(f"Failed to fetch data from {url}: status is {response.status_code}")
 
-        content = yaml.load(response.text, Loader=yaml.CLoader)
-
-        result = dict()
-        for data in collect_schema_data(content):
-            result[data.interface_method] = SpecMethod.create(data)
-        return result
+        return Parser.parse_from_raw(response.text)
 
     @staticmethod
     def parse_from_file(file_path: str) -> Dict[str, SpecMethod]:
         try:
             with open(file_path) as f:
-                content = yaml.load(f, Loader=yaml.CLoader)
-
-            result = dict()
-            for data in collect_schema_data(content):
-                result[data.interface_method] = SpecMethod.create(data)
-
-            return result
-
+                content = f.read()
+            return Parser.parse_from_raw(content)
         except FileNotFoundError:
             raise ValueError(f"Failed to open file {file_path}: file not found")
