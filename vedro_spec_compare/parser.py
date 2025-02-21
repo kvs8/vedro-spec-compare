e0a25510d52015086944a3eda1180c3b6a7fa669
kvs8 <kostya.shefer.999@gmail.com>
2025-02-21 14:47:55 +0700

    Add response status codes

--- vedro_spec_compare/parser.py
+++ vedro_spec_compare/parser.py
@@ -1,59 +1,28 @@
-from typing import Any, Dict, List
-
 import requests
 import yaml
-from schemax import SchemaData, collect_schema_data
+from schemax import collect_schema_data
 
-
-class SpecMethod:
-    def __init__(
-            self, method: str,
-            route: str,
-            query_params: List[str],
-            request_body_schema: Dict[str, Any],
-            response_codes: List[str],
-            response_body_schema: Dict[str, Any]
-    ):
-        self.method = method
-        self.route = route
-        self.query_params = query_params
-        self.body_request_schema = request_body_schema
-        self.response_codes = response_codes
-        self.response_schema = response_body_schema
-
-    @staticmethod
-    def create(data: Any) -> "SpecMethod":
-        if isinstance(data, SchemaData):
-            return SpecMethod(
-                method=data.http_method,
-                route=data.path,
-                query_params=data.queries,
-                request_body_schema=data.request_schema,
-                response_codes=[str(data.status)],
-                response_body_schema=data.response_schema
-            )
-        else:
-            raise ValueError("Unsupported data format")
+from .models import APIMethods, RawSpecMethod
 
 
 class Parser:
     @classmethod
-    def parse(cls, spec_path: str) -> Dict[str, SpecMethod]:
+    def parse(cls, spec_path: str) -> APIMethods:
         if spec_path.startswith("http://") or spec_path.startswith("https://"):
             return cls.parse_from_url(spec_path)
         else:
             return cls.parse_from_file(spec_path)
 
     @staticmethod
-    def parse_from_raw(content: str) -> Dict[str, SpecMethod]:
+    def parse_from_raw(content: str) -> APIMethods:
         data = yaml.load(content, Loader=yaml.CLoader)
-        result = dict()
+        api_methods = APIMethods({})
         for item in collect_schema_data(data):
-            result[item.interface_method] = SpecMethod.create(item)
-        return result
+            api_methods.add_method(RawSpecMethod.create(item))
+        return api_methods
 
     @staticmethod
-    def parse_from_url(url: str) -> Dict[str, SpecMethod]:
+    def parse_from_url(url: str) -> APIMethods:
         response = requests.get(url)
 
         if response.status_code != 200:
@@ -62,7 +31,7 @@ class Parser:
         return Parser.parse_from_raw(response.text)
 
     @staticmethod
-    def parse_from_file(file_path: str) -> Dict[str, SpecMethod]:
+    def parse_from_file(file_path: str) -> APIMethods:
         try:
             with open(file_path) as f:
                 content = f.read()
